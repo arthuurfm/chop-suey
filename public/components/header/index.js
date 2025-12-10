af93678a34215bc1b87b7fe98f7a119081b3868b
Arthur Franz <arthurfranzmoura@gmail.com>
2025-12-10 10:54:56 -0300

    colors, components and reset

---
+++ public/components/header/index.js
@@ -0,0 +1,13 @@
+const header = document.createElement('header');
+
+header.innerHTML = `
+  <div class='header-container'> 
+    <nav>
+      <h1>Chop Suey!</h1>
+      <span>Entregador</span>
+      <span>Restaurante</span>
+    </nav>
+  </div>
+`;
+
+export default header;
---
+++ public/index.js
@@ -0,0 +1,4 @@
+import header from './components/header/index.js';
+
+const body = document.querySelector('body');
+body.appendChild(header);
