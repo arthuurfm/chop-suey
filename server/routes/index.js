0cf1b76d7e709e8f5d2c459dd517513014502c6e
Arthur Franz <arthurfranzmoura@gmail.com>
2025-12-11 17:52:47 -0300

    dinamic routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,15 +1,34 @@
+import fs from 'fs';
 import path from 'path';
 import url from 'url';
 import express from 'express';
 
-const currentPath = url.fileURLToPath(import.meta.url);
-const publicDirectory = path.join(currentPath, '../../..', 'public');
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function routes(app) {
-  app.get('/register', (req, res) => {
-    res.sendFile(path.join(publicDirectory, 'pages/register/index.html'));
-  });
+  // diretório public.
+  const publicDirectory = path.join(__dirname, '../..', 'public');
+  // diretório pages.
+  const pagesDirectory = path.join(publicDirectory, 'pages');
 
+  // lê todas as pastas dentro de public/pages.
+  const folders = fs.readdirSync(pagesDirectory);
+
+  folders.forEach(folder => {
+    // caminho da pasta.
+    const folderPath = path.join(pagesDirectory, folder);
+    // se caminho da pasta tem index.html.
+    const indexFile = path.join(folderPath, 'index.html');
+
+    // se existir, envia a rota.
+    if (fs.existsSync(indexFile)) {
+      app.get(`/${folder}`, (req, res) => {
+        res.sendFile(indexFile);
+      });
+    }
+  });
+  
   app.use(express.static(publicDirectory));
 }
 
